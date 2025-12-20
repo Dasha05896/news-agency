8ed265cd358a3c4bf9e8eceffc5934b682ab7771
Daria Budovich <Budovich_Dasha@ukr.net>
2025-12-20 20:21:59 +0200

    Configure admin panel for Topic, Redactor and Newspaper models

--- agency/admin.py
+++ agency/admin.py
@@ -5,4 +5,31 @@ from .models import Redactor, Newspaper, Topic
 
 @admin.register(Redactor)
 class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
 
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_display = ("title", "topic", "published_date")
+    list_filter = ("topic",)
+
+
+admin.site.register(Topic)
